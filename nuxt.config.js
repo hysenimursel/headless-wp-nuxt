10c13b778e9619e803d363caec434bc626492978
hysenimursel <hysenimursel@gmail.com>
2021-03-26 19:11:27 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ export default {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
         hid: "description",
+        og: ":href="`blog/${post.slug}`",
         name: "description",
         content: process.env.npm_package_description || ""
       }
